5adca695e8cdf7940bc1255ec46283f73c966cb7
ali <aliwolfi18@gmail.com>
2021-06-03 20:55:48 -0700

    trying styled components

--- src/ui/SearchingCity.tsx
+++ src/ui/SearchingCity.tsx
@@ -1,30 +1,56 @@
 import {SearchIcon} from "@heroicons/react/outline";
-import {useState} from "react";
-import {forwardRefWithAs} from "@headlessui/react/dist/utils/render";
-
+import { useRef, useState} from "react";
+import styled from "styled-components";
 
 export function SearchingCity() {
 
-    const [isActive, setIsActive] = useState(false);
+    const buttonEl = useRef<HTMLButtonElement>();
 
-    return (
-        <div>
-            <h1 className='text-gray-100 font-medium tracking-wide text-4xl text-center pt-8 '>Weather App</h1>
-            <div className='flex justify-center mt-16'>
-                <div
-                    className='group flex border-b-2 border-blue-200 w-64 focus-within:border-blue-400 focus-within:text-blue-400 text-gray-100'>
-                    <SearchIcon
-                        className='h-5 w-5 mr-2.5 mt-0.5'
-                    />
-                    <input onClick={() => setIsActive(true)}
-                           className='bg-transparent pb-1 focus:outline-none w-full text-gray-100' type="text"
-                           placeholder='City..'/>
-                </div>
-                <button
-                    className='px-6 py-1 bg-blue-300 rounded ml-16 focus:outline-none transform hover:-translate-y-1 transition ring-2 ring-opacity-40'>
-                    <span className=''>Search</span>
-                </button>
-            </div>
-        </div>
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    const handleOnMouseMove = (e) => {
+        setX(e.pageX - buttonEl.current.offsetLeft);
+        setY(e.pageY - buttonEl.current.offsetTop);
+    }
+
+    const h1 = styled.h1`
+    
+    `
+
+    return(
+        <>
+
+        </>
     )
+
 }
+
+// return (
+//     <>
+//         <h1 className='text-gray-100 font-medium tracking-wide text-4xl text-center pt-8 '>Weather App</h1>
+//         <div className='flex justify-center mt-16'>
+//             <div
+//                 className='group flex border-b-2 border-blue-200 w-64 focus-within:border-blue-400 focus-within:text-blue-400 text-gray-100'>
+//                 <SearchIcon
+//                     className='h-5 w-5 mr-2.5 mt-0.5'
+//                 />
+//                 <input className='bg-transparent pb-1 focus:outline-none w-full text-gray-100' type="text"
+//                        placeholder='City..'/>
+//             </div>
+//
+//                 <button
+//                     style={beforeButtonEl}
+//                     ref={buttonEl}
+//                     className='relative inline-flex px-8 py-2.5 bg-gray-700 tracking-wide overflow-hidden btn-p text-white'
+//                     onMouseMove={handleOnMouseMove}
+//                 >
+//                     <div
+//                         style={{top: `${y}px`, left: `${x}px`}}
+//                         className='absolute w-1 h-1 rounded-full bg-blue-300 btn transform -translate-x-1/2 -translate-y-1/2'
+//                     />
+//                     <span style={{zIndex: 1}} className='relative'>Search</span>
+//                 </button>
+//         </div>
+//     </>
+// )
