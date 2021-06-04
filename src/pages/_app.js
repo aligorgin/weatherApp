b8769e78b2535c9af50170e47c61d4354cd89ad5
ali <aliwolfi18@gmail.com>
2021-06-04 04:23:22 -0700

    added themes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,15 @@
-import '../../styles/globals.css'
+import '../../styles/globals.css';
+import {ThemeProvider} from "styled-components";
+import GlobalStyles from "../theme/globalStyles";
+import Theme from "../theme/theme";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return (
+        <ThemeProvider theme={Theme}>
+            <GlobalStyles/>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
 
 export default MyApp;
--- src/ui/SearchingCity.tsx
+++ src/ui/SearchingCity.tsx
@@ -1,6 +1,6 @@
 import {SearchIcon} from "@heroicons/react/outline";
-import { useRef, useState} from "react";
-import styled from "styled-components";
+import {useRef, useState} from "react";
+
 
 export function SearchingCity() {
 
@@ -14,43 +14,34 @@ export function SearchingCity() {
         setY(e.pageY - buttonEl.current.offsetTop);
     }
 
-    const h1 = styled.h1`
-    
-    `
 
-    return(
+    return (
         <>
+            <h1 className='text-gray-100 font-medium tracking-wide text-4xl text-center pt-8 '>Weather App</h1>
+            <div className='flex justify-center mt-16'>
+                <div
+                    className='group flex border-b-2 border-blue-200 w-64 focus-within:border-blue-400 focus-within:text-blue-400 text-gray-100'>
+                    <SearchIcon
+                        className='h-5 w-5 mr-2.5 mt-0.5'
+                    />
+                    <input className='bg-transparent pb-1 focus:outline-none w-full text-gray-100' type="text"
+                           placeholder='City..'/>
+                </div>
 
+                <button
+                    ref={buttonEl}
+                    className={`relative inline-flex px-8 py-2.5 bg-gray-700 tracking-wide overflow-hidden btn-p text-white`}
+                    onMouseMove={handleOnMouseMove}
+                >
+                    <span style={{zIndex: 1}} className='relative'>Search</span>
+                    <div
+                        style={{top: `${y}px`, left: `${x}px`}}
+                        className='absolute w-1 h-1 rounded-full bg-blue-300 btn transform -translate-x-1/2 -translate-y-1/2'
+                    />
+                </button>
+            </div>
         </>
     )
 
 }
 
-// return (
-//     <>
-//         <h1 className='text-gray-100 font-medium tracking-wide text-4xl text-center pt-8 '>Weather App</h1>
-//         <div className='flex justify-center mt-16'>
-//             <div
-//                 className='group flex border-b-2 border-blue-200 w-64 focus-within:border-blue-400 focus-within:text-blue-400 text-gray-100'>
-//                 <SearchIcon
-//                     className='h-5 w-5 mr-2.5 mt-0.5'
-//                 />
-//                 <input className='bg-transparent pb-1 focus:outline-none w-full text-gray-100' type="text"
-//                        placeholder='City..'/>
-//             </div>
-//
-//                 <button
-//                     style={beforeButtonEl}
-//                     ref={buttonEl}
-//                     className='relative inline-flex px-8 py-2.5 bg-gray-700 tracking-wide overflow-hidden btn-p text-white'
-//                     onMouseMove={handleOnMouseMove}
-//                 >
-//                     <div
-//                         style={{top: `${y}px`, left: `${x}px`}}
-//                         className='absolute w-1 h-1 rounded-full bg-blue-300 btn transform -translate-x-1/2 -translate-y-1/2'
-//                     />
-//                     <span style={{zIndex: 1}} className='relative'>Search</span>
-//                 </button>
-//         </div>
-//     </>
-// )
